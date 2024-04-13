7c9762187db10f6d8c8fce63498f2de60b49e370
Yash Varma <yash.v220104@gmail.com>
2024-04-13 08:03:35 +0530

    new changes

--- js/script.js
+++ js/script.js
@@ -8,3 +8,16 @@ document.querySelectorAll('.image-container img').forEach(image => {
 document.querySelector('.pop-image span').onclick = () => {
     document.querySelector('.pop-image').style.display = 'none';
 }
+
+// Add a click event listener to the chatbot icon
+document.querySelector('.chatbot-icon').addEventListener('click', function() {
+    document.querySelector('.chatbot').classList.toggle('show');
+});
+
+// Close the chatbot when clicking outside of it
+document.addEventListener('click', function(event) {
+    if (!event.target.closest('.chatbot-wrapper')) {
+        document.querySelector('.chatbot').classList.remove('show');
+    }
+});
+
